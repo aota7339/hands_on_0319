89fbe689c3c251ab04f154f9c3c29905ed30dd8a
aota7339 <122717773+aota7339@users.noreply.github.com>
2023-03-19 15:09:45 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,24 +1,11 @@
 const http = require('node:http');
-const messages = [];
 
 const server = http.createServer((request, response) => {
-  response.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-
-  if (request.url === "/favicon.ico" || request.url === "/robots.txt") {
-    // 何も返さない
-  } else if (request.url === "/") {
-    response.write("メッセージを送ってください。");
-  } else {
-    messages.push(request.url);
-    
-    let text = "";
-    for (const message of messages) {
-      text += `${decodeURIComponent(message).slice(1)}\n`;
-    }
-
-    response.write(text);
-  }
-
+  response.setHeader('Content-Type', 'text/html; charset=UTF-8'); // 変更: plain->html
+  response.write(`
+    <script type="module">
+    </script>
+  `);
   response.end();
 });
 
